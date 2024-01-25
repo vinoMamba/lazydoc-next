8ec0e6394c373bedff3d250c4457186be1db770a
wangxin <vino0908@outlook.com>
2024-01-25 13:05:57 +0800

    feat: theme toggle tool

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,4 +1,5 @@
 import { HomeNav } from "@/components/home-nav";
+import { ThemeMode } from "@/components/theme-mode";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
@@ -10,13 +11,16 @@ export default function HomeLyout({ children }: Props) {
   return (
     <div className=" flex flex-col min-h-screen">
       <header className=" container bg-background z-40">
-        <div className=" flex items-center h-20 justify-between">
+        <div className="flex items-center h-20 justify-between">
           <HomeNav />
-          <Button asChild variant="secondary">
-            <Link href="/login">
-              Sign In
-            </Link>
-          </Button>
+          <div className=" flex items-center gap-2">
+            <ThemeMode />
+            <Button asChild variant="secondary">
+              <Link href="/login">
+                Sign In
+              </Link>
+            </Button>
+          </div>
         </div>
       </header>
       <main className="flex-1">{children}</main>
