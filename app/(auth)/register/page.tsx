a215db3c78269204845547d32692efc5f1d033a1
wangxin <vino0908@outlook.com>
2024-01-29 13:07:22 +0800

    去掉prisma

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,16 +1,10 @@
+import { RegisterForm } from "@/components/register-form";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
-import { redirect } from "next/navigation";
 
 export default function Register() {
-  const handleSubmit = async () => {
-    'use server'
-    redirect('/login');
-  }
   return (
 
     <div className="container h-screen flex items-center justify-center">
@@ -25,21 +19,7 @@ export default function Register() {
           <h1>Sign Up</h1>
         </CardHeader>
         <CardContent>
-          <form action={handleSubmit}>
-            <div className=" grid w-full gap-4 items-center">
-              <div className=" flex flex-col space-y-1.5">
-                <Label htmlFor="email">Email</Label>
-                <Input id="email" name="email" type="email" placeholder="Input your email" />
-              </div>
-              <div className=" flex flex-col space-y-1.5">
-                <Label htmlFor="password">Password</Label>
-                <Input id="password" name="password" type="password" placeholder="Input your password" />
-              </div>
-              <div className="flex justify-center">
-                <Button type="submit" className=" w-full">Sign Up</Button>
-              </div>
-            </div>
-          </form>
+          <RegisterForm />
         </CardContent>
         <CardFooter>
           <p className="text-center text-sm text-gray-600">
---
+++ components/register-form.tsx
@@ -0,0 +1,73 @@
+"use client"
+import type { FC } from 'react'
+import { z } from 'zod'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from 'react-hook-form'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+
+
+const formSchema = z.object({
+  email: z.string().email({ message: 'Invalid email address' }),
+  password: z.string().min(6, { message: 'Password must be at least 6 characters long' }).max(100, { message: 'Password must be at most 100 characters long' })
+})
+
+export const RegisterForm: FC = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    }
+  })
+
+  const onSubmit = form.handleSubmit(async (data: z.infer<typeof formSchema>) => {
+    const res = await fetch('api/user/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data),
+    })
+    const json = await res.json()
+    console.log(json)
+  })
+
+  return (
+    <Form {...form}>
+      <form className='space-y-4' onSubmit={onSubmit}>
+        <FormField
+          control={form.control}
+          name='email'
+          render={({ field }) => {
+            return (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input {...field} placeholder='Input your Email' />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )
+          }
+          }
+        />
+        <FormField
+          control={form.control}
+          name='password'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input {...field} placeholder='Input your password' type='password' />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className=' w-full'>Sign Up</Button>
+      </form>
+    </Form>
+  )
+}
